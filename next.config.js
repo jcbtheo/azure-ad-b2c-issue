8aa0aac5b8ee2a814daf1a511c6fc1ce11a43bd2
jcbtheo <44957929+jcbtheo@users.noreply.github.com>
2022-08-05 13:44:04 -0500

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    AZURE_AD_B2C_CLIENT_ID: process.env.AZURE_AD_B2C_CLIENT_ID,
+    AZURE_AD_B2C_CLIENT_SECRET: process.env.AZURE_AD_B2C_CLIENT_SECRET,
+    AZURE_AD_B2C_TENANT_NAME: process.env.AZURE_AD_B2C_TENANT_NAME,
+    AUTH_TENANT_GUID: process.env.AUTH_TENANT_GUID,
+    JWT_SECRET: process.env.JWT_SECRET,
+    AZURE_AD_B2C_PRIMARY_USER_FLOW: process.env.AZURE_AD_B2C_PRIMARY_USER_FLOW,
+  },
+};
---
+++ pages/signout.tsx
@@ -0,0 +1,9 @@
+import { signOut } from "next-auth/react";
+
+export default function Signout() {
+  if (typeof window !== "undefined") {
+    signOut({ callbackUrl: "/" });
+  }
+
+  return null;
+}
